57ceba46af17828cf664ddb5ee189265de801c6b
evaserman <ellieav@mit.edu>
2022-02-28 10:07:18 -0500

    commit

--- SQL Basics.sql
+++ SQL Basics.sql
@@ -1,8 +1,17 @@
 -- SELECT Name, (Students*1000) AS Population
 -- FROM Colleges
 -- WHERE City = 'Cambridge'
+-- SELECT FirstName,
+	-- LastName,
+    -- Birthdate,
+    -- timestampdiff(YEAR, Birthdate, now()) AS Age
+-- FROM Students
+
 SELECT FirstName,
-	LastName,
-    Birthdate,
-    timestampdiff(YEAR, Birthdate, now()) AS Age
+		LastName
 FROM Students
+WHERE Region = 'TX';
+
+SELECT *
+FROM Students
+WHERE Phone IS NOT NULL
