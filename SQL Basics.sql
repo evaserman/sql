b3edc82c33d826136da29c883ddb7cb51f24b82d
evaserman <ellieav@mit.edu>
2022-02-28 10:01:54 -0500

    commit

--- SQL Basics.sql
+++ SQL Basics.sql
@@ -1,3 +1,8 @@
-SELECT *
-FROM Colleges
-WHERE City = 'Cambridge'
+-- SELECT Name, (Students*1000) AS Population
+-- FROM Colleges
+-- WHERE City = 'Cambridge'
+SELECT FirstName,
+	LastName,
+    Birthdate,
+    timestampdiff(YEAR, Birthdate, now()) AS Age
+FROM Students
