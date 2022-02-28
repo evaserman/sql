771c227eebad7b9fa2b7586434b04f8ec4e3f47f
evaserman <ellieav@mit.edu>
2022-02-28 10:35:56 -0500

    commit

--- SQL Basics 02.sql
+++ SQL Basics 02.sql
@@ -4,8 +4,15 @@
 	-- Email LIKE '%gmail%'
     -- OR
     -- Phone LIKE '%541%'
-SELECT S.StudentID, S.FirstName, S.LastName,
-	C.CollegeID, C.Name
-FROM Students S
-INNER JOIN Colleges C
-ON S.CollegeID = C.CollegeID
+-- SELECT FirstName, LastName,
+	-- FriendID
+-- FROM Students AS t1
+-- INNER JOIN Students AS t2
+-- ON t1.FriendID = t2.FriendID
+
+SELECT*
+FROM Books B
+JOIN CollegeBooks cb
+	ON B.BookID = cb.BookID
+JOIN Colleges C
+	ON C.CollegeID = cb.BookID
